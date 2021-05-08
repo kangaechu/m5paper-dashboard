becbe0a9d87b5cbc5fc3d4d3aff5ded9038d730c
kangaechu <kangae2@gmail.com>
2021-05-08 11:37:15 +0900

    print hello

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <Dashboard />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import Dashboard from "./components/Dashboard.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-})
+    Dashboard,
+  },
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ frontend/src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    hello
+  </div>
+</template>
+  
+  <script lang="ts">
+  import { ref, defineComponent } from 'vue'
+  export default defineComponent({
+    name: 'Dashboard',
+  })
+  </script>
+ 
+  <style scoped>
+  </style>
+  
