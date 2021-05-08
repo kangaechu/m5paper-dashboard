b54b128cda61cb025254506dac8dcf4f3ea7ff22
kangaechu <kangae2@gmail.com>
2021-05-08 21:45:54 +0900

    フォントのサイズを拡大

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,16 +1,17 @@
 <template>
-  <div>
-    hello
-  </div>
+  <div class="dashboard">hello</div>
 </template>
   
   <script lang="ts">
-  import { ref, defineComponent } from 'vue'
-  export default defineComponent({
-    name: 'Dashboard',
-  })
-  </script>
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  name: "Dashboard",
+});
+</script>
  
-  <style scoped>
-  </style>
+<style scoped>
+div.dashboard {
+  font-size: 10em;
+}
+</style>
   
